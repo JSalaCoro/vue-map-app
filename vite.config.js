90f0db4b9092fac07fe033541508ba51e993d504
JSalaCoro <jsalacor@gmail.com>
2024-12-23 15:42:46 +0100

    deployment stuf

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/vue-map-app/',
   plugins: [
     vue(),
     vueDevTools(),
