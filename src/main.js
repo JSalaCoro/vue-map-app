e4773cc8030df65a44c25c785251a4a80eb009ee
JSalaCoro <jsalacor@gmail.com>
2024-12-23 15:01:36 +0100

    first commit

---
+++ src/main.js
@@ -0,0 +1,9 @@
+import './assets/main.css'
+
+import { createApp } from 'vue'
+import AppMap from './AppMap.vue'
+
+//createApp(App).mount('#app')
+const app = createApp(AppMap)
+app.mount('#app')
+
