314d8f693423353d68655459ef355f6fb9df5965
JSalaCoro <jsalacor@gmail.com>
2025-01-10 11:32:46 +0100

    secondary axis for yearly chart

--- src/components/LandingsYearlyChartComponent.vue
+++ src/components/LandingsYearlyChartComponent.vue
@@ -18,25 +18,44 @@ export default {
     return {
       data: {
         labels: NEP_cat_yearly_data.data.map(row => row.year),
-        datasets: [{ 
+        datasets: [{
+            label: 'Kg',
             data: NEP_cat_yearly_data.data.map(row => row.kg),
             backgroundColor: "#7BBC8C",
-            borderColor: "#7BBC8C"           
-            }]
+            borderColor: "#7BBC8C",
+            yAxisID: 'y'          
+            },
+            {
+            label: 'Eu',
+            data: NEP_cat_yearly_data.data.map(row => row.eu),
+            backgroundColor: "#496CAD",
+            borderColor: "#496CAD",
+            yAxisID: 'y1'
+            }
+          ]
       },
       options: {
         responsive: true,
         indexAxis: 'x',
-        plugins: {legend: {display: false},
+        plugins: {legend: {display: true, position:'bottom'},
                   title: {
                     text:'Species landings evolution (Catalunya)',
                     display:true
                   }},
-        scales: {y:{title: {text:'Kg', display: true}}},
+        scales: {
+          y:{
+            display: true,
+            title: {text:'Kg', display: true}},
+          y1: {
+            display: true,
+            title: {text:'Eu', display:true},
+            position: 'right'
+          }},
         maintainAspectRatio: false
+        }
       }
     }
   }
-}
+
   </script>
   
